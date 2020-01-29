f8d9bed9b72a8a152e831a6324e3289c75be1fc4
Misaki Koonce and Trevor Mackin <misaki@>
2020-01-29 13:16:06 -0800

    Render api call result in View.

--- MvcApiCall/Startup.cs
+++ MvcApiCall/Startup.cs
@@ -49,7 +49,7 @@ namespace MvcApiCall
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
